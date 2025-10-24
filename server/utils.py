da1e569aa24124b27499dd28219634b854abfc68
Andrey Kovshov <andy.kovshov@gmail.com>
2025-10-24 21:41:35 +0700

    Bogdan commit

--- server/utils.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-def get_user_reports(user_id: str) -> list:
-    
-    logger.info(f"Заглушка: Запрос метрик для пользователя {user_id}")
-    return []
